b7faff1f07616d68c4d4c183a4ca7b569b2b0b2a
Tzvetomir Todorov <ttodorov@locqus.com>
2015-07-13 13:46:17 -0400

    Add functional test for login form error and header home link

--- links/tests/functional/test_authentication.py
+++ links/tests/functional/test_authentication.py
@@ -47,3 +47,24 @@ class AuthenticationTest(FunctionalTest):
 	@unittest.skip('Silly test.')	
 	def test_login_stylesheet(self):
 		self.check_stylesheet('login')
+		
+	def test_user_cant_login_and_returns_to_homepage(self):
+		# Bo wants to Login to Bounce
+		self.browser.get(self.live_server_url + '/login')
+
+		# He enters an invalid username and password and clicks the 'Login' button
+		self.browser.find_element_by_id('id_username').send_keys('invalid_user')
+		self.browser.find_element_by_id('id_password').send_keys('invalid_password')
+		login_button = self.browser.find_element_by_id('submit')
+		login_button.click()
+		time.sleep(5)
+
+		# The login form responds with an error message
+		error_message = self.browser.find_element_by_css_selector('p.error').text
+		self.assertIn("Your username and password didn't match.", error_message)
+
+		# Bo can't remember his login info and decides to just browse without Logging in
+		home_link = self.browser.find_element_by_css_selector('header>h1>a')
+		home_link.click()
+		time.sleep(5)
+		self.assertEqual(self.browser.current_url, self.live_server_url + '/')
